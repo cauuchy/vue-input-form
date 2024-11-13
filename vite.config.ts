e91c815b99367d1ee13bd40a7d82225244fc2894
cauchy <cauuchy@gmail.com>
2024-11-14 06:54:28 +0900

    github pages test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/github-pages-test/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/vue-input-form/' : './',
   plugins: [
     vue(),
     vueDevTools(),
